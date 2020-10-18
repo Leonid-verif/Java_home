0c349dcb48fdf320db7d6cd6216e0ec413bc4dff
Leonid <leonid.minfin@gmail.com>
2020-10-19 00:53:09 +0300

    1,2 completed

--- src/main/java/lesson1/Task1.java
+++ src/main/java/lesson1/Task1.java
@@ -2,15 +2,36 @@ package lesson1;
 
 public class Task1 {
     /* Минимум трех чисел
-    Написать функцию, которая вычисляет минимум из трёх чисел.
-    */
+Написать функцию, которая вычисляет минимум из трёх чисел.
+*/
+//    public static void main(String[] args) {
+//        System.out.println(min(5, 2, 3));
+//    }
+//
+//    public static int min(int a, int b, int c) {
+//        return a <= b && a <= c ? a : b <= a && b <= c ? b : c;
+
+//
     public static void main(String[] args) {
-        //System.out.println(min(5,2,3));
-    }
+        int a = 5;
+        int b = 2;
+        int c = 3;
+        int min;
 
-    public static int min(int a, int b, int c) {
-        return 0;
-    }
-}
+        if (a < b && a < c) {
+            min=a;
+            System.out.println("Min: " + min);
+        }
+        else if (b < a && b < c) {
+            min=b;
+            System.out.println("Min: "+ min);
+        }
+        else {
+            min=c;
+            System.out.println("Min: " + min);
+        }
 
-//test commit
+    }
+    }
+//}
+
